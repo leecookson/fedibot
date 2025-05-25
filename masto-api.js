a9470f170663161a182180a0cc214d79cf25d861
Lee Cookson <1561172+leecookson@users.noreply.github.com>
2025-05-25 13:15:04 -0400

    Update masto-api.js

--- masto-api.js
+++ masto-api.js
@@ -3,7 +3,7 @@ import Mastodon from 'mastodon-api';
 var M = new Mastodon({
   timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
   api_url: 'https://hachyderm.io/api/v1/',
-  access_token: '7WIi1DRssJ41g6snfytAx3tHqnb-2hm2O9czTI0pV8s'
+  access_token: process.env.MASTODON_ACCESS_KEY
 })
 
 const stream = M.stream('streaming/user')
